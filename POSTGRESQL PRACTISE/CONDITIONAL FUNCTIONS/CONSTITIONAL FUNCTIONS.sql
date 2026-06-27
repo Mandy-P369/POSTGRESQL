86f163ae3733ce108474d1cd4988fe9f132a5ec2
aayushmann pateriya <aayushpateriya762@gmail.com>
2026-06-27 23:40:00 +0530

    Add files via upload

--- POSTGRESQL PRACTISE/CONDITIONAL FUNCTIONS/CONSTITIONAL FUNCTIONS.sql
+++ POSTGRESQL PRACTISE/CONDITIONAL FUNCTIONS/CONSTITIONAL FUNCTIONS.sql
@@ -3,40 +3,32 @@ Select count(*) from traindata where gender ='F';
 Select count(*) from traindata where gender = 'M' ;
 Select * from testdata ;
 
-SELECT column_name, data_type
-FROM information_schema.columns 
-join information_schema.constraints
-WHERE table_name = 'traindata';
+create extension POSTGIS;
 
 Select count(distinct cc_num) from traindata ;
-
-Select count(distinct cc_num) from testdata ;
-
-Select count(distinct trans_num) from traindata ;
-
-ALTER TABLE traindata
-ADD CONSTRAINT pk_trans_num
-PRIMARY KEY (trans_num);
+Select distinct state from testdata ;
 
 
 SELECT
-    c.column_name,
-    c.data_type,
-    tc.constraint_type
-FROM information_schema.columns c
-LEFT JOIN information_schema.key_column_usage kcu
-    ON c.table_name = kcu.table_name
-   AND c.column_name = kcu.column_name
-LEFT JOIN information_schema.table_constraints tc
-    ON kcu.constraint_name = tc.constraint_name
-   AND kcu.table_name = tc.table_name
-WHERE c.table_name = 'traindata'
-  AND c.table_schema = 'public';
+    CASE
+        WHEN state IN ('CT','ME','MA','NH','NJ','NY','PA','RI','VT')
+            THEN 'North East'
+        WHEN state IN ('IA','IL','IN','KS','MI','MN','MO','ND','NE','OH','SD','WI')
+            THEN 'Mid West'
+        WHEN state IN ('AL','AR','DC','FL','GA','KY','LA','MD','MS','NC','OK','SC','TN','TX','VA','WV')
+            THEN 'South'
+        WHEN state IN ('AK','AZ','CA','CO','HI','ID','MT','NM','NV','OR','UT','WA','WY')
+            THEN 'West'
+        ELSE 'Other'
+    END AS Regions,
+    ROUND(SUM(amt)::numeric, 2) AS Total_Amount,
+	 DENSE_RANK() OVER (
+        ORDER BY SUM(amt) DESC
+    ) AS "Rank of Regions"
+FROM traindata
+GROUP BY Regions
+ORDER BY Total_Amount DESC;
 
+Select * from testdata  order by amt desc ;
 
-
-ALTER TABLE testdata
-ADD CONSTRAINT fk_trans_num
-FOREIGN KEY (transaction_id)
-REFERENCES traindata(trans_num);
 
